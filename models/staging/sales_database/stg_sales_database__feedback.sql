a10983b560163b1e3e88ffb6b93b36f527ea79c4
BPlpl <basile.pleple@gmail.com>
2024-11-18 23:12:27 +0100

    commit column_5

--- models/staging/sales_database/stg_sales_database__feedback.sql
+++ models/staging/sales_database/stg_sales_database__feedback.sql
@@ -1,5 +1,6 @@
 select
  CONCAT(feedback_id, '_', order_id) AS feedback_id,
+ 5 as column_5
  order_id,
  feedback_score,
  DATETIME(feedback_form_sent_date, "Europe/Paris") AS feedback_form_sent_at,
